1f66bd9a3b8a8f52cdb050780217aa10c1abac50
Mark Prichard <mqprichard@gmail.com>
2015-02-03 10:02:05 -0800

    Added users

--- src/main/java/com/appdynamics/demo/AcmeCartScenarioTwoTest.java
+++ src/main/java/com/appdynamics/demo/AcmeCartScenarioTwoTest.java
@@ -54,12 +54,34 @@ public class AcmeCartScenarioTwoTest extends  AcmeSessionLoadTest{
     String getUsername() {
         String user;
 
-        switch (randomGen.nextInt(5)) {
+        switch (randomGen.nextInt(26)) {
             case 0: user = "test"; break;
-            case 1: user = "vikash"; break;
-            case 2: user = "santo"; break;
-            case 3: user = "ravi"; break;
-            case 4: user = "root"; break;
+            case 1: user = "appdynamics"; break;
+            case 2: user = "val.chibisov@appdynamics.com"; break;
+            case 3: user = "bhaskar.sunkara@appdynamics.com"; break;
+            case 4: user = "mark.prichard@appdynamics.com"; break;
+            case 5: user = "adam.leftik@appdynamics.com"; break;
+            case 6: user = "sid.choudhury@appdynamics.com"; break;
+            case 7: user = "rey.ong@appdynamics.com"; break;
+            case 8: user = "mahesh.gandhe@appdynamics.com"; break;
+            case 9: user = "nima haddadkaveh@appdynamics.com"; break;
+            case 10: user = "ariel.smollar@appdynamics.com"; break;
+            case 11: user = "amod.gupta@appdynamics.com"; break;
+            case 12: user = "omed.habib@appdynamics.com"; break;
+            case 13: user = "ian.mcguinness@appdynamics.com"; break;
+            case 14: user = "harish.nataraj@appdynamics.com"; break;
+            case 15: user = "ian.withrow@appdynamics.com"; break;
+            case 16: user = "shiv.loka@appdynamics.com"; break;
+            case 17: user = "akankshu.dhawan@appdynamics.com"; break;
+            case 18: user = "jeff.morgan@appdynamics.com"; break;
+            case 19: user = "pamela.clark@appdynamics.com"; break;
+            case 20: user = "steve.hetland@appdynamics.com"; break;
+            case 21: user = "lynn.davidson@appdynamics.com"; break;
+            case 22: user = "ellen.evans@appdynamics.com"; break;
+            case 23: user = "jacquie.finney@appdynamics.com"; break;
+            case 24: user = "charles.smith@appdynamics.com"; break;
+            case 25: user = "eric.mackay@appdynamics.com"; break;
+            case 26: user = "byron.martin@appdynamics.com"; break;
             default: user = "test"; break;
         }
         return user;
